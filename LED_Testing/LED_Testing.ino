36df1c2590099b267ab83ce919b6b80ca2e04969
Koo <kootg04@gmail.com>
2017-02-05 19:11:05 -0800

    Streaking works

--- LED_Testing/LED_Testing.ino
+++ LED_Testing/LED_Testing.ino
@@ -2,31 +2,27 @@
 
 #define PIN 6
 Adafruit_NeoPixel strip;
-int amount = 136;
-int matrix[amount];
+int amount = 141;
+int i = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   strip = Adafruit_NeoPixel(amount, PIN, NEO_GRB + NEO_KHZ800);
   strip.begin();
   strip.setPixelColor(amount, 0,0,0);
   strip.show();
 }
 
-void forwardMatrix() {
-  int prev;
-  int lit = 0;
-  for( int i = 0; i < amount; i++) {
-    if(lit == 0 && 
-
-    prev = matrix[i];
-  }  
-}
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  int itr = 0;
-  for(int i = 0; i < amount; i++) {
+  if(i >= amount) {
+    i = 0;
+  }
+  if(i < amount) {
+    if(i >= 5) {
+      strip.setPixelColor((i-5), 0, 0, 0);
+      strip.show();
+    }
     strip.setPixelColor(i, 255, 0, 0);
     strip.show();
+    i++;
   }
 }
